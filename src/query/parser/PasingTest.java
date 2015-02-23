c9fd3a7539ffe529b98c3547b37707998e3e8cbd
yonghyun-cho <yonghyun.cho@samsung.com>
2015-02-23 23:34:38 +0900

    20150223 마무리

--- src/query/parser/PasingTest.java
+++ src/query/parser/PasingTest.java
@@ -5,12 +5,27 @@ import query.parser.vo.ConstInfo;
 import query.parser.vo.QueryInfo;
 import query.parser.vo.SubQueryInfo;
 
+/*
+ * 
+SELECT EMP.ENAME, EMP.SAL, DEPT.DNAME, (SELECT EMP.SAL FROM EMP WHERE EMPNO = 7839) FROM EMP, (SELECT DEPT.DNAME, DEPT.DEPTNO, DEPT.LOC FROM DEPT WHERE DEPTNO.SIZE > 100) DEPT WHERE EMP.DEPTNO = DEPT.DEPTNO AND DEPT.LOC = 'CHICAGO'  
+===========================
+<< SUBQUERY >> (SELECT EMP.SAL FROM EMP WHERE EMPNO = 7839)
+----------------------------
+SELECT EMP.ENAME, EMP.SAL, DEPT.DNAME, 1_SubQuery_TEMP FROM EMP, (SELECT DEPT.DNAME, DEPT.DEPTNO, DEPT.LOC FROM DEPT WHERE DEPTNO.SIZE > 100) DEPT WHERE EMP.DEPTNO = DEPT.DEPTNO AND DEPT.LOC = 'CHICAGO'  
+
+<< SUBQUERY >> (SELECT DEPT.DNAME, DEPT.DEPTNO, DEPT.LOC FROM DEPT WHERE DEPTNO.SIZE > 100)
+----------------------------
+SELECT EMP.ENAME, EMP.SAL, DEPT.DNAME, 1_SubQuery_TEMP FROM EMP, 2_SubQuery_TEMP DEPT WHERE EMP.DEPTNO = DEPT.DEPTNO AND DEPT.LOC = 'CHICAGO'  
+===========================
+
+ */
+
 public class PasingTest {
 
 	public static void main(String[] args) throws Exception {
 		// 파일 입력 로직 변경 // 2015.02.12. 조용현
 		QueryParser qp = new QueryParser();
-		qp.readQueryTextFile("C:\\Users\\RHYH\\Documents\\testQuery.txt");
+		qp.readQueryTextFile("C:\\testQuery.txt");
 		
 //		qp.parsingQuery();
 //		
